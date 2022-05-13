0ee7e960a3a2a430f41796f666bc1896033660a0
cuihairu <chuihairu@gmail.com>
2022-05-13 17:46:49 +0800

    hello

---
+++ fix-missing-font.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+Ver=v0.1.0
+DownloadUrl=https://github.com/cuihairu/fix-wps-missing-fonts/releases/download/${Ver}/fonts.tar.xz
+FontHome=/usr/share/fonts/wps-office
+TempFile=/tmp/wps-fonts.tar.xz
+
+if [ ! -d "${FontHome}" ];then
+	sudo mkdir ${FontHome}
+fi 	
+
+wget -O ${TempFile} ${DownloadUrl} 
+
+if [ ! -f "${TempFile}" ];then
+	echo "download fail"
+	exit 1
+fi
+
+sudo tar -xvf ${TempFile} -C  --strip-components 1
+sudo chown root:root -R ${FontHome}
+sudo chmod 644 ${FontHome}/* 
+
+# build cache
+cd ${FontHome}
+sudo mkfontscale && mkfontdir && fc-cache -fv
+echo "fix success"
